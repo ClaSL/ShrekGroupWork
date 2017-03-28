abdc5788dbde1b5782c2bcf6d7ae5fb5ddb47a3b
Clarissa Schwarz-Linnet <clarissa.schwarz@gmx.net>
2017-03-28 14:59:56 +0200

    First button works (sound missing), second button as well, but Shrek is moving.

--- moves.js
+++ moves.js
@@ -8,6 +8,7 @@ let donkeyHero = document.querySelector("#donkey");
 let startMusic = document.querySelector("#startMusic");
 
 
+
 /*BUTTONS*/
 
 let startBtn = document.querySelector("#firstButton");
@@ -15,42 +16,56 @@ let peekBtn = document.querySelector("#peekButton");
 
 /*First Button: Start music and Shrek arrives*/
 
-function playAndArrive() {
+/*function playAndArrive() {
     console.log("Play and Arrive");
     startMusic.play();
     startMusic.addEventListener("ended", shrekArrive);
 
 }
+*/
+startBtn.addEventListener("click", shrekArrive);
 
 function shrekArrive() {
     shrekHero.classList.add("shrekMove");
+    console.log("It works!");
 }
 
+
 /*Remove First button*/
+/*
 shrekHero.addEventListener("animationend", removeShrekArrive);
 
 function removeShrekArrive() {
     console.log("First Step over");
     shrekHero.classList.remove("shrekMove");
 }
+*/
 
 /*Second Button: Donkey PEAKs in*/
 
-let dPeak = document.querySelector("#donkeyPeek");
-
-dPeak.addEventListener("click", peak);
+peekBtn.addEventListener("click", peak);
 
 function peak() {
-    console.log("Donkey is peaking!")
+    console.log("Donkey is peaking!");
+    let horseSound = document.querySelector("#horse");
+     horseSound.play();
     donkeyHero.classList.add("donkeyPeek");
+    horseSound.addEventListener("ended",shrekMirror);
+
+    function shrekMirror (){
+        console.log("What was that?");
+        shrekHero.classList.add("shrekMirror")
+    }
+
+
 }
 
 /*Remove PEAK Button*/
-
+/*
 donkeyHero.addEventListener("animationend", removePeak);
 
 function removePeak(){
     console.log("Shut up donkey");
     donkeyHero.classList.remove("donkeyPeek");
-
 }
+*/
